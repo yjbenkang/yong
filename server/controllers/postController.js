b3c3620d869adc2eb31ed80b0027db17176a2cc2
강용진 <kyongjin1234@gmail.com>
2021-09-14 17:15:45 +0900

    HTTP Method POST 게시물 시 유효성 검사 조건 추가

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -24,12 +24,20 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
+  if(!req.body.제목 || !req.body.내용){
+    // 400 Bad Request
+    res.status(400).send('제목 혹은 내용이 입력되지 않았습니다.');
+    return;
+  }
   const post = {
     id: posts.length+1,
     제목: req.body.제목,
     내용: req.body.내용
   };
-  console.log(post);
   posts.push(post);
   res.json(post);
 };
+// 나중에는 id는 데이터베이스에 의해 자동으로 할당된다.
+// Input Validation
+// 보안을 위해서는 절대 client가 보내는 데이터를 믿어서는 안된다.
+// 따라서 예를들어 이와 같이 (1) 이름은 있어야하고, (2) 길이는 반드시 3이상이여야 한다. 라는 조건을 걸어 유효성을 검사한다.
