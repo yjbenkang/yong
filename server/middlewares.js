e2c64eead599305d850c8b9b222ce42f38c93126
강용진 <kyongjin1234@gmail.com>
2021-09-20 15:38:04 +0900

    게시물 CREATE, UPDATE, DELETE 작업 시 로그아웃 상태 예외처리를 middleware로 교체

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,6 +1,21 @@
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.loggedInUser = req.session.user;
-    console.log(res.locals);
     next();
 }
+
+export const protectorMiddleware = (req, res, next) => {
+    if (req.session.loggedIn) {
+        return next();
+    } else {
+        return res.status(401).send("로그인이 필요합니다.")
+    }
+}
+
+export const publicOnlyMiddleware = (req, res, next) => {
+    if (!req.session.loggedIn) {
+        return next();
+    } else {
+        return res.status(400).send("로그아웃 시에만 가능합니다.")
+    }
+};
