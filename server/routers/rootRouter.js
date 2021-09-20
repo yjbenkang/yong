4075e2d402a9b38cada0a5f09079c92f4e2d7d56
강용진 <kyongjin1234@gmail.com>
2021-09-20 15:43:53 +0900

    user router, root router에 public||protect middleware 추가

--- server/routers/rootRouter.js
+++ server/routers/rootRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getJoin, postJoin, postLogin } from "../controllers/userController";
 import { home } from "../controllers/postController";
+import { publicOnlyMiddleware} from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.post("/login", postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter.post("/login", publicOnlyMiddleware, postLogin);
 
 export default rootRouter;
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { logout, postChangePassword, postEdit, remove, see } from "../controllers/userController";
+import { protectorMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.post("/edit", postEdit);
-userRouter.post("/change-password",postChangePassword);
+userRouter.post("/edit", protectorMiddleware, postEdit);
+userRouter.post("/change-password", protectorMiddleware, postChangePassword);
 userRouter.post("/remove", remove);
-userRouter.post("/logout", logout);
+userRouter.post("/logout", protectorMiddleware, logout);
 userRouter.get("/:id", see)
 
 export default userRouter;
